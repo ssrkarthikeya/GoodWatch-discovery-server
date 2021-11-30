ff8ad42c0aa4c31faa43f565d78c9bbb5ce0b0a5
ssrkarthikeya <Hachinov1>
2021-12-01 00:01:06 +0530

    initial

---
+++ src/main/java/io/icegadda/goodwatchdiscoveryserver/GoodwatchDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwatchDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwatchDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/icegadda/goodwatchdiscoveryserver/GoodwatchDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodwatchDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
