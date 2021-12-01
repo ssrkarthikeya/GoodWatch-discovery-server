a297c328a4ffa47713e474338f8407aae8ce40e1
ssrkarthikeya <Hachinov1>
2021-12-01 09:14:23 +0530

    level1 complete

--- src/main/java/io/icegadda/goodwatchdiscoveryserver/GoodwatchDiscoveryServerApplication.java
+++ src/main/java/io/icegadda/goodwatchdiscoveryserver/GoodwatchDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package io.icegadda.goodwatchdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GoodwatchDiscoveryServerApplication {
 
 	public static void main(String[] args) {
